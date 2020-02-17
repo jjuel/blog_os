fb939848afcd099ebf2717f681ff4ad743b2d407
Jordan Juel <Jordan.Juel@ameritas.com>
2020-02-17 16:33:42 -0600

    Up until completeing the Integration test in post 4

--- src/serial.rs
+++ src/serial.rs
@@ -6,7 +6,7 @@ lazy_static! {
     pub static ref SERIAL1: Mutex<SerialPort> = {
         let mut serial_port = unsafe { SerialPort::new(0x3F8) };
         serial_port.init();
-        Mutex::new(serial_port);
+        Mutex::new(serial_port)
     };
 }
 
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,28 +4,19 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use blog_os::println;
 use core::panic::PanicInfo;
 
-#[no_mangle] // Don't mangle the name of this function
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
